d98afc66fa08c177a0c41b860f5ca9074cbbf673
Viktoras <123058129+viktoras-v@users.noreply.github.com>
2026-01-28 13:22:57 +0200

    Rename S3 buckets to VV-demo-bucket variants

--- Homework_40/main.tf
+++ Homework_40/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "ca-devops-ua6-demo-bucket"
+  bucket = "VV-demo-bucket"
 }
 
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "ca-devops-ua6-demo-bucket2"
+  bucket = "VV-demo-bucket2"
 }
