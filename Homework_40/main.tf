0b957135c6c9845e99ca98aac6d6d059bc6b1264
Viktoras <123058129+viktoras-v@users.noreply.github.com>
2026-01-30 10:25:07 +0200

    Add force_destroy to S3 bucket resources

--- Homework_40/main.tf
+++ Homework_40/main.tf
@@ -5,7 +5,9 @@ resource "aws_s3_bucket" "bucket1" {
 
 resource "aws_s3_bucket" "bucket2" {
   bucket = "vvv-demo-bucket2"
+  force_destroy = true
 }
 resource "aws_s3_bucket" "bucket3" {
   bucket = "vvv-demo-bucket3"
+  force_destroy = true
 }
