147a6a3b1db79e8574bf1b65cf9c31ae11ad6a2a
Viktoras <123058129+viktoras-v@users.noreply.github.com>
2026-01-30 10:34:35 +0200

    Comment out S3 bucket resources
    Comment out S3 bucket resources for buckets 1, 2, and 3.

--- Homework_40/main.tf
+++ Homework_40/main.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "vvv-demo-bucket1"
-  force_destroy = true
-}
+#resource "aws_s3_bucket" "bucket1" {
+ # bucket = "vvv-demo-bucket1"
+  #force_destroy = true
+#}
 
 
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "vvv-demo-bucket2"
-  force_destroy = true
-}
-resource "aws_s3_bucket" "bucket3" {
-  bucket = "vvv-demo-bucket3"
-  force_destroy = true
-}
+#resource "aws_s3_bucket" "bucket2" {
+ # bucket = "vvv-demo-bucket2"
+  #force_destroy = true
+#}
+#resource "aws_s3_bucket" "bucket3" {
+ # bucket = "vvv-demo-bucket3"
+  #force_destroy = true
+#}
