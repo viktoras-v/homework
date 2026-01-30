23d01f5749bbb4d16f87862598ea382a5faec020
Viktoras <123058129+viktoras-v@users.noreply.github.com>
2026-01-30 14:00:10 +0200

    Update main.tf

--- Homework_40/main.tf
+++ Homework_40/main.tf
@@ -1,3 +1,24 @@
+provider "aws" {
+  region = "us-east-1"  # замени на нужный регион
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b6c6ebed2801a5cb"  # замени на свой AMI
+  instance_type = "t2.micro"
+  key_name      = "MyKeyPair"              # твой ключ
+
+  # Security Group
+  vpc_security_group_ids = ["sg-0821e286182681ca7"]
+
+  # public IP
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
+
+
 #resource "aws_s3_bucket" "bucket1" {
  # bucket = "vvv-demo-bucket1"
   #force_destroy = true
