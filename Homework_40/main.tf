97af2786def944b66ddb43ec2acce933724a3798
Viktoras <123058129+viktoras-v@users.noreply.github.com>
2026-01-30 15:47:23 +0200

    Comment out AWS instance resource in main.tf
    Comment out aws_instance resource and its properties.

--- Homework_40/main.tf
+++ Homework_40/main.tf
@@ -1,20 +1,20 @@
 
 
-resource "aws_instance" "example" {
-  ami           = "ami-0b6c6ebed2801a5cb"  # замени на свой AMI
-  instance_type = "t2.micro"
-  key_name      = "MyKeyPair"              # твой ключ
+#resource "aws_instance" "example" {
+#  ami           = "ami-0b6c6ebed2801a5cb"  # замени на свой AMI
+#  instance_type = "t2.micro"
+#  key_name      = "MyKeyPair"              # твой ключ
 
   # Security Group
-  vpc_security_group_ids = ["sg-0821e286182681ca7"]
+ # vpc_security_group_ids = ["sg-0821e286182681ca7"]
 
   # public IP
-  associate_public_ip_address = true
+  #associate_public_ip_address = true
 
-  tags = {
-    Name = "MyTerraformInstance"
-  }
-}
+  #tags = {
+   # Name = "MyTerraformInstance"
+  #}
+#}
 
 
 #resource "aws_s3_bucket" "bucket1" {
